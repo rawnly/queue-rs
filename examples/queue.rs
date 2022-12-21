48321ff192e8738f14e5b058fe5fed4484f3390e
rawnly <rawnly@users.noreply.github.com>
2022-12-21 03:11:42 +0100

    chore(examples/queue): better formatting

--- examples/queue.rs
+++ examples/queue.rs
@@ -1,27 +1,20 @@
 use queue_rs::prelude::*;
 use queue_rs::FifoQueue;
-use std::io::Write;
 use std::time::Duration;
 use std::{sync::Arc, thread};
 
-fn flush() {
-    let _ = std::io::stdout().flush();
-}
-
 fn main() {
     let queue = Arc::new(FifoQueue::<String>::new());
-    let queue1 = Arc::clone(&queue);
-    let queue2 = Arc::clone(&queue);
+    let queue1 = queue.clone();
+    let queue2 = queue.clone();
 
     let handle = thread::spawn(move || {
-        print!("\n child thread started");
-        flush();
+        println!("[CHILD_THREAD] Started");
         let dur = Duration::from_millis(20);
 
         loop {
             let t = queue1.pop();
-            print!("\n dequeued {} on child thread", t);
-            flush();
+            println!("[CHILD_THREAD] dequeued {}", t);
 
             if &t == "quit" {
                 break;
@@ -30,26 +23,21 @@ fn main() {
             thread::sleep(dur);
         }
 
-        print!("\n child thread shutting down");
-        flush();
+        println!("[CHILD_THREAD] quitting");
     });
 
     let dur = Duration::from_millis(20);
     for i in 0..10 {
         let msg = format!("msg #{}", i);
-        print!("\n enqueued {:?} on main thread", msg);
-        flush();
+        println!("[MAIN_THREAD] Enqueued {:?}", msg);
         queue2.push(msg);
         thread::sleep(dur);
     }
 
     let msg = "quit".to_string();
-    print!("\n enqueued {:?} on main thread", msg);
-    flush();
+    println!("[MAIN_THREAD] Enqueued {:?}", msg);
     queue2.push(msg);
 
-    print!("\n waiting for child process to stop");
-    flush();
+    println!("[MAIN_THREAD] Waiting for child process to stop");
     handle.join().unwrap();
-    print!("\n queue length = {}", queue2.len());
 }
